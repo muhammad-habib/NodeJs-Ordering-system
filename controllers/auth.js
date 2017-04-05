2ac8211ad5ae03eaefb17281cbfebeb0a540d19d
MoustafaElkady <moustafa.m.elkady@gmail.com>
2017-04-05 07:51:47 +0200

    Add node-jsonwebtoken

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,33 +1,35 @@
 var express = require("express");
 var router = express.Router();
+
 var mongoose = require("mongoose");
 var crypto = require('crypto'), shasum = crypto.createHash('sha1');
 var bodyParser = require("body-parser");
 
 var validator = require("validator");
+var jwt = require('jsonwebtoken');
 
 function sha256(msg) {
     return crypto.createHash("sha256").update(msg).digest("base64");
 }
 
 router.post("/login", function (request, response) {
-
+console.log(request.body);
     var email = request.body.email;
     var password = request.body.password;
 
     if (!validator.isEmail(email) || validator.isEmpty(email) || validator.isEmpty(password)) {
         response.status(400).json({error: "Wrong Data."});
     } else {
-        mongoose.model("users").find({email: email}, {password: true, access_token: true}, function (err, user) {
-            if (!err && sha256(password) == user[0].password) {
-                response.json(
-                    {
-                        id: user[0].id,
-                        name: user[0].name,
-                        email: user[0].email,
-                        token: user[0].access_token
-                    }
-                );
+        mongoose.model("users").findOne({email: email}, function (err, user) {
+            if (!err && user && sha256(password) === user.password) {
+                var userData = {
+                    id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    token: jwt.sign({ sub: user._id }, "123@321")
+                };
+                console.log(userData);
+                response.json(userData);
             }
             else {
                 response.status(400).json({error: "Invalid email or password."});
@@ -39,7 +41,6 @@ router.post("/login", function (request, response) {
 router.post("/register", bodyParser.urlencoded({extended: false}), function (request, response) {
 
     var UserModel = mongoose.model("users");
-    var access_token = sha256(request.body.name + new Date() + Math.random());
     var name = request.body.name;
     var email = request.body.email;
     var password = request.body.password;
