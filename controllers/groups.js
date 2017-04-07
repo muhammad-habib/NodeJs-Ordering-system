cbfba0f75c255d472fbec7f6ca4f98af0d9ef526
seifElislam <seif.eleslam1990@gmail.com>
2017-04-07 15:05:09 +0200

    add get & list members in group

--- controllers/groups.js
+++ controllers/groups.js
@@ -8,7 +8,7 @@ var bodyParser = require("body-parser");
 var validator = require("validator");
 var jwt = require('jsonwebtoken');
 
-router.get("/list", function(requset,response){
+router.get("/list", function(request,response){
   console.log("list");
   mongoose.model("groups").find({}, function (err, groups) {
       if(!err)
@@ -20,6 +20,53 @@ router.get("/list", function(requset,response){
       }
   });
   //response.json({g1:{name:"OS",id:2},g2:{name:"java",id:3}});
+
+});
+
+router.get("/:name/members",function(request,response){
+  console.log("list members");
+
+  // mongoose.model("groups").find({name:request.params.name}.populate("users"), function (err, groups) {
+  //     if(!err)
+  //     {
+  //         response.json(groups);
+  //     }
+  //     else {
+  //         response.status(400).json({error: err});
+  //     }
+  // });
+
+  mongoose.model("groups").find({name:request.params.name},{ _id:0,members: 1}).populate('users').exec(function (err, members) {
+    console.log(members);
+          // if (err)
+          //     response.json({error: "Not found"});
+          // else if(following.following.length == 1 &&  following.following[0] == null ) {
+          //     following.following.pop();
+          //     following.save();
+          //     response.json(following.following);
+          // }
+          // else {
+          //     response.json(following.following);
+          // }
+  })
+
+
+});
+
+
+router.get("/:name/members/add/:id",function(request,response){
+  console.log("list members");
+
+  mongoose.model("groups").update({name:request.params.name},{$push:{members:request.params.id}},{}, function (err, groups) {
+      if(!err)
+      {
+          response.json({"status": "done"});
+      }
+      else {
+          response.status(400).json({error: err});
+      }
+  });
+
 
 });
 
