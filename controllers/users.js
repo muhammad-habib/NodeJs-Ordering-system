3fe7c2420f020a5f47b7e57835c7a89c04d1e27d
muhammad-habib <muhammad.habib.dev@gmail.com>
2017-04-13 06:06:14 +0200

    remove blocked user from search

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-
+var helpers = require("../util/helpers");
 var mongoose = require("mongoose");
 var crypto = require('crypto'), shasum = crypto.createHash('sha1');
 var bodyParser = require("body-parser");
@@ -38,10 +38,16 @@ router.get("/search",function (request, response) {
             });
             break;
         case "email":
-            mongoose.model("users").find({email: request.query.q}, function (err, users) {
+            mongoose.model("users").find({email: request.query.q}, function (err, user) {
                 if(!err)
                 {
-                    response.json(users);
+                    console.log(user[0].blocking)
+                    if(!helpers.isInArray(request.query.user_id, user[0].blocking))
+                    {
+                        response.json(user);
+                    }else {
+                        response.json({});
+                    }
                 }
                 else {
                     response.status(400).json({error: err});
